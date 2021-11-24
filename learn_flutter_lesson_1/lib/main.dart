92c8548fdebccb53ac8e9a629aac65c8b137f1ad
flyingcanopy aka bhuvan mysore <bhuvanmysore1996@gmail.com>
2021-11-24 23:15:32 +0530

    word pair generator

--- learn_flutter_lesson_1/lib/main.dart
+++ learn_flutter_lesson_1/lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    double days = 200;
-    return MaterialApp(home: HomePage());
+    final wordPair = WordPair.random();
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[500]),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('WordPair Generator'),
+          ),
+          body: Center(
+            child: Text(wordPair.asPascalCase),
+          ),
+        ));
   }
 }
